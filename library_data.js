e296c0a741466eb0e6e2d334c484d123d7e0c9ca
msn <nmssm@daum.net>
2026-01-15 13:29:46 +0900

    feat: Initialize Kerygma Booklist - library data and page

---
+++ library_data.js
@@ -0,0 +1,69 @@
+const libraryData = [
+    {
+        "id": "1",
+        "title": "영혼론 (De Anima)",
+        "subtitle": "영혼의 불멸성에 관하여",
+        "author": "테르툴리아누스",
+        "translator": "조영호 감수",
+        "status": "published",
+        "tags": ["교부", "명저번역"],
+        "cover": "https://image.yes24.com/goods/134446549/XL", 
+        "link": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=354714138",
+        "desc": "초기 기독교 사상의 정수. 영혼의 기원과 본질에 대한 교부학적 탐구."
+    },
+    {
+        "id": "2",
+        "title": "교리사 개요",
+        "subtitle": "서방 교회의 맥락에서 교리의 발전",
+        "author": "아돌프 폰 하르낙",
+        "status": "published",
+        "tags": ["교리사", "역사신학"],
+        "cover": "https://image.yes24.com/goods/115166468/XL",
+        "link": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=303861875",
+        "desc": "교리의 역사적 변천 과정을 추적한 하르낙의 기념비적 저작의 요약본."
+    },
+    {
+        "id": "3",
+        "title": "기독교의 기본 진리",
+        "subtitle": "Fundamental Truths",
+        "author": "라인홀트 제베르크",
+        "status": "published",
+        "tags": ["조직신학", "현대신학"],
+        "cover": "https://image.yes24.com/goods/124021703/XL",
+        "link": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=330188686",
+        "desc": "기독교 신앙의 본질적 진리를 현대적 지성과의 대화 속에서 변증."
+    },
+    {
+        "id": "4",
+        "title": "마르틴 루터와 종교개혁의 기초",
+        "subtitle": "Martin Luther und die Grundlegung der Reformation",
+        "author": "아돌프 폰 하르낙",
+        "status": "published",
+        "tags": ["종교개혁", "루터"],
+        "cover": "https://image.yes24.com/goods/117726581/XL",
+        "link": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=312448373",
+        "desc": "종교개혁의 본질적 동기와 루터의 신학적 기초를 파헤친 역작."
+    },
+    {
+        "id": "5",
+        "title": "성탄절 칸타타 신앙",
+        "subtitle": "대림절 묵상집",
+        "author": "김재진",
+        "status": "published",
+        "tags": ["묵상", "절기"],
+        "cover": "https://image.yes24.com/goods/123805908/XL",
+        "link": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=329606782",
+        "desc": "성탄의 신비를 깊이 있게 묵상하도록 돕는 대림절 가이드."
+    },
+    {
+        "id": "6",
+        "title": "세례론",
+        "subtitle": "문제제기 및 해설",
+        "author": "김재진",
+        "status": "draft",
+        "tags": ["성례전", "케리그마"],
+        "cover": "", 
+        "link": "",
+        "desc": "현대 교회 세례론의 쟁점과 신학적 해설을 담은 연구서."
+    }
+];
