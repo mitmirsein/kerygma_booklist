27663e7ce01a54eacc305e7b2f6f2ebf791239bc
msn <nmssm@daum.net>
2026-01-15 14:13:43 +0900

    feat: Sync library data with Kyobo Books for high-res covers and correct links

--- library_data.js
+++ library_data.js
@@ -1,69 +1,68 @@
 const libraryData = [
-    {
-        "id": "1",
-        "title": "영혼론 (De Anima)",
-        "subtitle": "영혼의 불멸성에 관하여",
-        "author": "테르툴리아누스",
-        "translator": "조영호 감수",
-        "status": "published",
-        "tags": ["교부", "명저번역"],
-        "cover": "https://image.yes24.com/goods/134446549/XL", 
-        "link": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=354714138",
-        "desc": "초기 기독교 사상의 정수. 영혼의 기원과 본질에 대한 교부학적 탐구."
-    },
-    {
-        "id": "2",
-        "title": "교리사 개요",
-        "subtitle": "서방 교회의 맥락에서 교리의 발전",
-        "author": "아돌프 폰 하르낙",
-        "status": "published",
-        "tags": ["교리사", "역사신학"],
-        "cover": "https://image.yes24.com/goods/115166468/XL",
-        "link": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=303861875",
-        "desc": "교리의 역사적 변천 과정을 추적한 하르낙의 기념비적 저작의 요약본."
-    },
-    {
-        "id": "3",
-        "title": "기독교의 기본 진리",
-        "subtitle": "Fundamental Truths",
-        "author": "라인홀트 제베르크",
-        "status": "published",
-        "tags": ["조직신학", "현대신학"],
-        "cover": "https://image.yes24.com/goods/124021703/XL",
-        "link": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=330188686",
-        "desc": "기독교 신앙의 본질적 진리를 현대적 지성과의 대화 속에서 변증."
-    },
-    {
-        "id": "4",
-        "title": "마르틴 루터와 종교개혁의 기초",
-        "subtitle": "Martin Luther und die Grundlegung der Reformation",
-        "author": "아돌프 폰 하르낙",
-        "status": "published",
-        "tags": ["종교개혁", "루터"],
-        "cover": "https://image.yes24.com/goods/117726581/XL",
-        "link": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=312448373",
-        "desc": "종교개혁의 본질적 동기와 루터의 신학적 기초를 파헤친 역작."
-    },
-    {
-        "id": "5",
-        "title": "성탄절 칸타타 신앙",
-        "subtitle": "대림절 묵상집",
-        "author": "김재진",
-        "status": "published",
-        "tags": ["묵상", "절기"],
-        "cover": "https://image.yes24.com/goods/123805908/XL",
-        "link": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=329606782",
-        "desc": "성탄의 신비를 깊이 있게 묵상하도록 돕는 대림절 가이드."
-    },
-    {
-        "id": "6",
-        "title": "세례론",
-        "subtitle": "문제제기 및 해설",
-        "author": "김재진",
-        "status": "draft",
-        "tags": ["성례전", "케리그마"],
-        "cover": "", 
-        "link": "",
-        "desc": "현대 교회 세례론의 쟁점과 신학적 해설을 담은 연구서."
-    }
+  {
+    "id": "1",
+    "title": "영혼론 (De Anima)",
+    "subtitle": "영혼의 불멸성에 관하여",
+    "author": "테르툴리아누스",
+    "status": "published",
+    "tags": ["영혼론", "교부", "조영호 감수"],
+    "cover": "https://contents.kyobobook.co.kr/sih/fit-in/800x1146/pdt/480D250597350.jpg",
+    "link": "https://ebook-product.kyobobook.co.kr/dig/epd/ebook/E000011414027",
+    "desc": "초기 기독교 사상의 정수. '라틴 신학의 아버지' 테르툴리아누스가 파헤친 영혼의 기원과 본질에 대한 기독교 최초의 체계적인 논의를 담고 있습니다."
+  },
+  {
+    "id": "2",
+    "title": "교리사 개요",
+    "subtitle": "서방 교회의 맥락에서 교리의 발전",
+    "author": "아돌프 폰 하르낙",
+    "status": "published",
+    "tags": ["교리사", "역사신학", "명저번역"],
+    "cover": "https://contents.kyobobook.co.kr/sih/fit-in/800x1146/pdt/480D241277510.jpg",
+    "link": "https://ebook-product.kyobobook.co.kr/dig/epd/ebook/E000010549804",
+    "desc": "교리의 역사적 변천 과정을 추적한 하르낙의 기념비적 저작. 테르툴리아누스부터 루터까지 서방 신학의 흐름을 인물과 논쟁 중심으로 짚어냅니다."
+  },
+  {
+    "id": "3",
+    "title": "기독교의 기본 진리",
+    "subtitle": "Fundamental Truths",
+    "author": "라인홀트 제베르크",
+    "status": "published",
+    "tags": ["조직신학", "현대신학", "명저번역"],
+    "cover": "https://contents.kyobobook.co.kr/sih/fit-in/800x1146/pdt/480D241130890.jpg",
+    "link": "https://ebook-product.kyobobook.co.kr/dig/epd/ebook/E000009815931",
+    "desc": "기독교 신앙의 본질적 진리를 현대적 지성과의 대화 속에서 변증합니다. 복잡한 논쟁 대신 신앙의 핵심과 힘을 명확하고 간결하게 전달하는 안내서입니다."
+  },
+  {
+    "id": "4",
+    "title": "마르틴 루터와 종교개혁의 기초",
+    "subtitle": "Martin Luther und die Grundlegung der Reformation",
+    "author": "아돌프 폰 하르낙",
+    "status": "published",
+    "tags": ["루터", "종교개혁", "현대신학"],
+    "cover": "https://contents.kyobobook.co.kr/sih/fit-in/800x1146/pdt/480D241103840.jpg",
+    "link": "https://ebook-product.kyobobook.co.kr/dig/epd/ebook/E000009965073",
+    "desc": "종교개혁의 본질적 동기와 루터의 신학적 기초를 파헤친 역작. 루터의 영적 고뇌와 투쟁을 통해 기독교 정신의 토대를 재조명합니다."
+  },
+  {
+    "id": "5",
+    "title": "성탄절 칸타타 신앙",
+    "subtitle": "대림절 묵상집",
+    "author": "김재진",
+    "status": "published",
+    "tags": ["묵상", "성탄절", "김재진"],
+    "cover": "https://contents.kyobobook.co.kr/sih/fit-in/800x1146/pdt/480D241091760.jpg",
+    "link": "https://ebook-product.kyobobook.co.kr/dig/epd/ebook/E000009119448",
+    "desc": "성경 인물들의 고난과 신앙 여정을 통해 성탄절의 진정한 의미를 깨닫게 하는 묵상집입니다. 하나님의 임마누엘 은혜를 발견하도록 이끌어줍니다."
+  },
+  {
+    "id": "6",
+    "title": "세례론",
+    "subtitle": "문제제기 및 해설",
+    "author": "김재진",
+    "status": "published",
+    "tags": ["세례", "성례전", "김재진"],
+    "cover": "https://contents.kyobobook.co.kr/sih/fit-in/800x1146/pdt/480D240655670.jpg",
+    "link": "https://ebook-product.kyobobook.co.kr/dig/epd/ebook/E000008021698",
+    "desc": "현대 교회 세례론의 주요 쟁점과 신학적 해설을 담은 연구서입니다. 세례의 본질과 실천에 대한 깊이 있는 통찰을 제공합니다."
+  }
 ];
